2b26cadd0f71cd9380318a3205c7010aa89861ba
Yasmin <yasminalvesdesouza27@gmail.com>
2023-10-10 23:10:09 -0300

    Feat: Integração com Banco de Dados

--- todolist/src/main/java/br/com/yasminvic/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/yasminvic/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.yasminvic.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/cadastro")
     //@RequestBody é pra dizer que o dado vai vir do body (formulario de cadastro)
-    public void create(@RequestBody UserModel user){
-        System.out.println(user.name);
+    public ResponseEntity create(@RequestBody UserModel user){
+        var username = this.userRepository.findByUsername(user.getUsername());
+        
+        if(username != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Funcionário já cadastrado");
+        }
+        
+        var userCreated = this.userRepository.save(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
     
 
--- todolist/src/main/java/br/com/yasminvic/todolist/user/UserModel.java
+++ todolist/src/main/java/br/com/yasminvic/todolist/user/UserModel.java
@@ -1,8 +1,37 @@
 package br.com.yasminvic.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity(name = "tb_users") //nome da tabela no bd
+@Data // responsavel por criar Getters e Setter da classe
 public class UserModel {
     
-    public String username;
-    public String name;
-    public String password;
+    @Id
+    @GeneratedValue(generator = "UUID") //gerar hash automático
+    private UUID id; //tipo um hash, sequencia de letras e números
+
+    @Column(unique = true) //faz com que só possa ter um username por conta
+    private String username;
+    private String name;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt; //local que foi criado
 }
+
+
+/** Se quisermos que tal classe tenha só Getters, colocar @Getter em cima da classe
+ * Se quisermos que tal classe tenha só Setter, colocar @Setter em cima da classe
+ * Se quisermos os dois, colocar @Data em cima da classe
+ * 
+ * Se quisermos definir algo pra um atributo espeficico, colocar @ em cima dele e dizer qual dos 3 tipos quer
+ */
